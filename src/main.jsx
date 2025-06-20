ba7a31ff0751a316bcf4aa1eaeec2430d5f58cac
osesunday1 <ose.sunday@gmail.com>
2025-06-20 08:54:48 -0600

    down

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom'; // 🔁 change this line
 import App from './App.jsx';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import { Provider } from 'react-redux';
 import store from './store/store.js';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.render( // 🔁 change this block
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
     </Provider>
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
